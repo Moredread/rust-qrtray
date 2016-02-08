83b58aba80af06ed19856ef54670aca0b5729ec9
André-Patrick Bubel <code@andre-bubel.de>
2016-02-08 15:12:32 +0100

    Minimal GTK program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate gtk;
+
+use std::process::exit;
+use gtk::traits::*;
+use gtk::signal::Inhibit;
+
 fn main() {
-    println!("Hello, world!");
+    if gtk::init().is_err() {
+        println!("Couldn't inititalize GTK");
+        exit(-1);
+    }
+
+    let window = gtk::Window::new(gtk::WindowType::Toplevel).unwrap();
+
+    window.set_default_size(400, 400);
+
+    window.connect_delete_event(|_, _| {
+        gtk::main_quit();
+        Inhibit(false)
+    });
+
+    window.show_all();
+    gtk::main();
 }
